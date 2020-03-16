9552d6aaa1f0807d8687964c107409b5b671bc82
harckline <antoinecronier1@gmail.com>
2020-03-16 14:53:42 +0100

    demo module1

---
+++ Module1Demo/Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1Demo
+{
+    public class Class1
+    {
+        #region A ne pas faire
+        public int prop1;
+
+        // Version Java
+        private int prop2;
+
+        public void setProp2(int prop2)
+        {
+            this.prop2 = prop2;
+        }
+
+        public int getProp2()
+        {
+            return this.prop2;
+        }
+        #endregion
+
+        #region Properties
+        // Utiliser les snippets
+        // propfull
+
+        //Variable de classe privé.
+        private int myVar;
+
+        // Propriété de myVar pour l'exposition publique.
+        public int MyProperty
+        {
+            get { return myVar; }
+            set {
+                if (value > 10)
+                {
+                    myVar = 10;
+                }
+                else
+                {
+                    myVar = value;
+                }
+            }
+        }
+
+        // prop
+        // Auto-property
+        public int MyProperty1 { get; set; }
+
+        // Readonly prop
+        public int MyProperty2 { get; }
+        #endregion
+
+        #region Constructeur
+        public Class1 (int test)
+        {
+            this.MyProperty = test;
+        }
+
+        //Snippet constructeur vide ctor
+        public Class1()
+        {
+            this.MyProperty2 = 5;
+        }
+        #endregion
+    }
+}
---
+++ Module1Demo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1Demo
+{
+    /// <summary>
+    /// Main class with entry point.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Entry point of my application.
+        /// </summary>
+        /// <param name="args">Command line args.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            int a = 1;
+            float b = 2.1F;
+            double c = 2.2;
+
+            bool d = true;
+
+            string e = "zoejroizje";
+
+            String f = e;
+            Console.WriteLine("int.MaxValue " + int.MaxValue);
+            Console.WriteLine("int.MinValue " + int.MinValue);
+
+            if (false)
+            {
+
+            }
+            else if (true)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            int i = 0;
+            while (i < 10)
+            {
+                i++;
+            }
+
+            for (int j = 0; j < 10; j++)
+            {
+
+            }
+
+            Class1 class1 = new Class1();
+            Class1 class11 = new Class1(1);
+
+            Console.WriteLine(class1.MyProperty1);
+            class1.MyProperty1 = 10;
+            Console.WriteLine(class1.MyProperty1);
+
+            // Read only prop
+            //class1.MyProperty2 = 10;
+
+            //C# règles:
+            // Fonction commence par une majuscule => PascalCase
+            // Il n'existe pas vraiment de primitive, tout est objet
+            // Tout les objets possède une valeur par défaut, forcé par C#
+
+            Console.ReadKey();
+        }
+    }
+}
