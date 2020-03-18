6518e9e7119534c82896c2aefe2bf51d3049b19f
harckline <antoinecronier1@gmail.com>
2020-03-18 16:50:31 +0100

    TPModule5-1

---
+++ Module4Demo/Controllers/PersonneController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Module4Demo.Controllers
+{
+    public class PersonneController : Controller
+    {
+        // GET: Personne
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Personne/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Personne/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personne/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Personne/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Personne/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Personne/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Personne/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Module4Demo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Module4Demo.MvcApplication" Language="C#" %>
