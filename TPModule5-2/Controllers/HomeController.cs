875d2709f1e93547efa9b534a1f63fad3ec32dd1
harckline <antoinecronier1@gmail.com>
2020-03-19 15:54:53 +0100

    Module5 TP2

---
+++ TPModule5-2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPModule5_2_BO;
+
+namespace TPModule5_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Pizza pizza;
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
